b8c869dcf135e59f9ac6ce781c8e62e5fa6330bb
Brian Tzu-Pin Ho <k333hoisi@gmail.com>
2026-03-29 22:58:41 +0800

    test(web): initialize pinia in app mount spec (#101)

--- apps/web/src/app/App.spec.ts
+++ apps/web/src/app/App.spec.ts
@@ -1,12 +1,13 @@
 import { describe, it, expect } from 'vitest';
 import router from '../router';
 import { mount } from '@vue/test-utils';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 
 describe('App', () => {
   it('renders properly', async () => {
     const wrapper = mount(App, {
-      global: { plugins: [router] },
+      global: { plugins: [createPinia(), router] },
     });
     await router.isReady();
     expect(wrapper.text()).toContain('Simple Social Media');
